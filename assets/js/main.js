c7270c8a40c1a39a1d4a463e4e20f84a533a7421
Rannojit <cserannojitsarkar22@gmail.com>
2020-10-10 21:52:10 +0600

    slider prev and next common design fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,6 +174,50 @@ google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
+
+if( $('.racesRunningSlider').length ){
+    $('.racesRunningSlider').slick({
+      dots: false,
+      infinite: false,
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
+
 /*----- end of Rannojit -----*/
 
 
