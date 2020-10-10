faeef355e1e8916998e92f86203d1ed457d70391
Milon9787 <56488424+Milon9787@users.noreply.github.com>
2020-10-11 00:07:16 +0600

    revent page slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -240,7 +240,84 @@ if( $('.freeAlcoholSlider').length ){
 
 /*----- end of Shariful -----*/
 
+/*
+-----------------------
+Start event Google Map ->> 
+-----------------------
+*/
 
+if( $('#eventgooglemap').length ){
+    var latitude = $('#eventgooglemap').data('latitude');
+    var longitude = $('#eventgooglemap').data('longitude');
+
+    var myCenter= new google.maps.LatLng(latitude,  longitude);
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    function initialize(){
+        var mapProp = {
+          center:myCenter,
+
+          mapTypeControl:false,
+          scrollwheel: false,
+
+          zoomControl: false,
+          disableDefaultUI: true,
+          zoom:17,
+          streetViewControl: false,
+          rotateControl: false,
+          mapTypeId:google.maps.MapTypeId.ROADMAP,
+          styles : CustomMapStyles
+      };
+      var map= new google.maps.Map(document.getElementById('eventgooglemap'),mapProp);
+
+      var marker= new google.maps.Marker({
+        position:myCenter,
+        icon:'assets/images/map-marker.png'
+        });
+      marker.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+}
+
+if( $('.relevant-evnt-slider').length ){
+    $('.relevant-evnt-slider').slick({
+      dots: false,
+      infinite: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
 
 /*----- end of Milon -----*/
 
